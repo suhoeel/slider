2dbd13d2cd220f0502e287acc096dd4cc2f7c8b5
suho <suho@oliveunion.com>
2022-05-16 19:38:16 +0900

    qweqwe

--- app/src/main/java/com/portfolio/myapplication/MainActivity.kt
+++ app/src/main/java/com/portfolio/myapplication/MainActivity.kt
@@ -2,13 +2,33 @@ package com.portfolio.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
 class MainActivity : AppCompatActivity() {
+
+    private val parentJob = SupervisorJob()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val scope = CoroutineScope(Dispatchers.Main + parentJob)
+        scope.launch {
+            val a: Job = launch {
+                Log.d("TEST", "a")
+                delay(2000L)
+            }
 
+            val b: Job = launch {
+                delay(2000L)
+                Log.d("TEST", "b")
+            }
+
+        }
+//            Log.d("TEST", "${this.coroutineContext}")
 
     }
+
 }
--- app/src/main/java/com/portfolio/myapplication/VerticalSlider.kt
+++ app/src/main/java/com/portfolio/myapplication/VerticalSlider.kt
@@ -1,10 +1,12 @@
 package com.portfolio.myapplication
 
-import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.*
+import android.graphics.drawable.ColorDrawable
 import android.util.AttributeSet
+import android.util.Log
 import android.util.TypedValue
+import android.view.MotionEvent
 import android.view.View
 import android.widget.FrameLayout
 
@@ -16,16 +18,34 @@ class VerticalSlider @JvmOverloads constructor(
     private var maskBitmap: Bitmap? = null
     private var offscreenBitmap: Bitmap? = null
     private var offscreenCanvas: Canvas? = null
+
     private var paint: Paint
     private var maskPaint: Paint
     private var cornerRadius = 100f
 
-    private val verticalUpDownSlideView: SliderView
+    private var mWidth = 0
+    private var mHeight = 0
 
-    private var yPos = 500f
+    private var yPos: Float = 0f
     private var volume = 0
 
+    companion object {
+        private const val STEP = 10
+    }
+
+    private var stepInterval = 0f
+
+    private val sliderSubView: SliderView
+
+    private var backgroundColor: Int? = null
+
+    //    private var yPos = 0f
+
+
     init {
+        if (background is ColorDrawable) {
+            backgroundColor = (background as ColorDrawable).color
+        }
         val metrics = context.resources.displayMetrics
         cornerRadius = TypedValue.applyDimension(
             TypedValue.COMPLEX_UNIT_DIP,
@@ -33,11 +53,12 @@ class VerticalSlider @JvmOverloads constructor(
             metrics
         )
         paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        paint.color = Color.RED
         maskPaint =
             Paint(Paint.ANTI_ALIAS_FLAG or Paint.FILTER_BITMAP_FLAG)
         maskPaint.xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR)
         setWillNotDraw(false)
-        verticalUpDownSlideView = SliderView(context)
+        sliderSubView = SliderView(context)
 
 
         if (attrs != null) {
@@ -46,9 +67,10 @@ class VerticalSlider @JvmOverloads constructor(
                 attrs,
                 R.styleable.VerticalSlider, defStyleAttr, 0
             )
-            /*  a.getDrawable(R.styleable.VerticalSlider_arcColor)?.let {
-                  backgroundDisabled = it
-              }
+            a.getColor(R.styleable.VerticalSlider_seekbarColor, Color.YELLOW).let {
+                sliderSubView.setHandlerColor(it)
+            }
+            /*
               a.getInt(R.styleable.ImageCircleButton_imagePadding, 20).let {
                   padding = it.toFloat()
               }
@@ -58,22 +80,64 @@ class VerticalSlider @JvmOverloads constructor(
             a.recycle()
         }
 
-        this.addView(verticalUpDownSlideView)
+        this.addView(sliderSubView)
     }
 
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        mWidth = w
+        mHeight = h
+        stepInterval = ((mHeight / STEP).toFloat())
+
+        Log.d("VIEW", "$w, $h")
+
+    }
+
+    override fun onInterceptTouchEvent(ev: MotionEvent): Boolean {
+        when (ev.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+            MotionEvent.ACTION_UP -> {
+
+            }
+            MotionEvent.ACTION_MOVE -> {
+                Log.d("VIEW", "1")
+            }
+        }
+        return super.onInterceptTouchEvent(ev)
+    }
+
+    override fun onTouchEvent(ev: MotionEvent): Boolean {
+        when (ev.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+            MotionEvent.ACTION_UP -> {
+
+            }
+            MotionEvent.ACTION_MOVE -> {
+                Log.d("VIEW", "${ev.y}")
+                sliderSubView.setYPos(ev.y)
+            }
+        }
+        return true
+    }
 
     override fun draw(canvas: Canvas) {
-        if (offscreenBitmap == null) {
+        if (maskBitmap == null) {
+            maskBitmap = createMask(width, height)
             offscreenBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
             offscreenCanvas = Canvas(offscreenBitmap!!)
         }
         super.draw(offscreenCanvas)
 
-        if (maskBitmap == null) {
-            maskBitmap = createMask(width, height)
-        }
         offscreenCanvas!!.drawBitmap(maskBitmap!!, 0f, 0f, maskPaint)
         canvas.drawBitmap(offscreenBitmap!!, 0f, 0f, paint)
+        /*offscreenCanvas!!.drawRect(0f, 0f, width.toFloat(), height.toFloat(), Paint().apply {
+            style = Paint.Style.FILL
+            color = Color.RED
+        })*/
     }
 
 
@@ -93,7 +157,7 @@ class VerticalSlider @JvmOverloads constructor(
         return mask
     }
 
-    fun setYPos(y: Float): Int {
+    /*fun setYPos(y: Float): Int {
 
         this.yPos = y
         var drawPos = y
@@ -120,39 +184,33 @@ class VerticalSlider @JvmOverloads constructor(
         verticalUpDownSlideView.setYPos(drawPos)
         invalidate()
         return volume
-    }
+    }*/
 
-    fun setYPosWithVol(y: Float, volume: Int): Int {
 
-        this.yPos = y
+    private fun setYPosWithVol(volume: Int): Int {
+
+//        this.yPos = y
+
         this.volume = volume
 
-        var drawPos = y
-
-        val height = height.toFloat()
-
-        if (yPos < 0) {
-            yPos = 0f
-        } else if (yPos > height) {
-            yPos = height
-        }
-
+//        setYPosWithVol(height - (volume * stepInterval), volume)
+        var drawPos = mHeight - (volume * stepInterval)
 
         when (volume) {
-            0 -> drawPos = height
+            0 -> drawPos = height.toFloat()
+            10 -> drawPos = 0f
         }
 
-        verticalUpDownSlideView.setYPos(drawPos)
-        invalidate()
+        sliderSubView.setYPos(drawPos)
+//        invalidate()
         return volume
     }
 
 
-
-    fun setVolume(vol: Int) {
+    /*fun setVolume(vol: Int) {
         val aStep: Float = (height / 10.0f)
         setYPosWithVol(height - (vol * aStep), vol)
-    }
+    }*/
 
 
     fun getVolume(): Int {
@@ -160,35 +218,77 @@ class VerticalSlider @JvmOverloads constructor(
     }
 
     fun setHandlerColor(color: String) {
-        verticalUpDownSlideView.setHandlerColor(color)
+        sliderSubView.setHandlerColor(color)
     }
 
     fun setHandlerColor(color: Int) {
-        verticalUpDownSlideView.setHandlerColor(color)
+        sliderSubView.setHandlerColor(color)
     }
 
     private inner class SliderView @JvmOverloads constructor(
         context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
     ) : View(context, attrs, defStyleAttr) {
 
-        private var yPos: Float = 3000f
-
         private var paint: Paint = Paint()
 
+        private var textPaint: Paint = Paint()
+
+        private var newBitmap: Bitmap? = null
+        private var newCanvas: Canvas? = null
+
         init {
-            paint.style = Paint.Style.FILL
-            paint.color = Color.GRAY
+//            paint.style = Paint.Style.FILL
+            paint.color = Color.BLUE
             paint.isAntiAlias = false
+            textPaint.color = Color.BLACK
+            textPaint.style = Paint.Style.STROKE
+            textPaint.textSize = 15f
+            textPaint.isAntiAlias = false
         }
 
+        /*    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+                super.onSizeChanged(w, h, oldw, oldh)
+                mWidth = w
+                mHeight = w
+            }
+    */
         override fun onDraw(canvas: Canvas) {
+
+            /*if (newBitmap == null) {
+                newBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+                newCanvas = Canvas(newBitmap!!)
+            }*/
             super.onDraw(canvas)
-            canvas.drawRect(0f, height.toFloat(), width.toFloat(), yPos, paint)
+
+
+            canvas.drawColor((if(backgroundColor == null) Color.WHITE else backgroundColor!!))
+//            Log.d("VIEW", "yPos ${yPos}")
+            Log.d("VIEW", "height ${height.toFloat()}")
+            canvas.drawRect(0f, yPos, width.toFloat(), height.toFloat(), this.paint)
+            canvas.drawText("0", (width / 4f), (height/3f), this.textPaint)
+
+//            canvas.drawBitmap(newBitmap!!, 0f, 0f, this.paint)
+
+//            offscreenCanvas!!.drawRect(0f, yPos, width.toFloat(), height.toFloat(), this@VerticalSlider.paint)
+
         }
 
         fun setYPos(y: Float) {
-            this.yPos = y
-            super.invalidate()
+            yPos = y
+            volume = (10 - (yPos / stepInterval)).toInt()
+
+            if (yPos < 0) {
+                volume = 10
+                yPos = 0f
+            } else if (yPos > mHeight) {
+                volume = 0
+                yPos = mHeight.toFloat()
+            }
+            Log.d("VIEW", "volume $volume")
+
+            this@VerticalSlider.invalidate()
+
+//            super.invalidate()
         }
 
         fun setHandlerColor(color: String) {
